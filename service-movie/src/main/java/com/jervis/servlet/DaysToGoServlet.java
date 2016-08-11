0770cc889e4ccc13d7ced74da35eb00cb0073a84
Jevis <ydj_jervis@foxmail.com>
2016-08-11 10:50:38 +0800

    Run-->Edit config...-->添加TomCat库就可以，Tomcat里面添加servlet api就不报错了

--- service-movie/src/main/java/com/jervis/servlet/DaysToGoServlet.java
+++ service-movie/src/main/java/com/jervis/servlet/DaysToGoServlet.java
@@ -1,12 +1,15 @@
 package com.jervis.servlet;
 
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DaysToGoServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println(new DateUtils().daysToNewYear() + " to go in 2014. Learn Gradle now!");
+//        resp.getWriter().println(new DateUtils().daysToNewYear() + " to go in 2014. Learn Gradle now!");
     }
 }
